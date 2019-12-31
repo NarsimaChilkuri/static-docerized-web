5d4227c8f02f61e45d4246d94deb2ae96da5f1e7
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2019-12-31 15:09:40 +0530

    some changes added

--- flask-app.py
+++ flask-app.py
@@ -35,6 +35,7 @@ def create_table():
        domain=domain_generator()
        cursor.execute("select count(*) from git_log")
        rowcount = cursor.fetchone()[0] + 1
+       
        print commit_id
        print user_name
        print user_email
@@ -45,6 +46,7 @@ def create_table():
        print commit_message
        print timestamp
        print port
+
        query = "INSERT INTO git_log (user_email, user_name, branch_name, commit_hash, domain_name,port,files_added,files_modified,files_removed,commit_message,timestamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
        values = (user_email,user_name,branch,commit_id,domain,port,listToString(files_added),listToString(files_modified),listToString(files_removed),commit_message,timestamp)
        cursor.execute(query,values)
