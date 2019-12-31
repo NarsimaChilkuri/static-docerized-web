3f3066431b393c4ba8fd028817793d200c387b80
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2019-12-31 15:08:40 +0530

    some changes added

--- flask-app.py
+++ flask-app.py
@@ -33,6 +33,8 @@ def create_table():
        client.images.push("74744556/static-web-page",commit_id)
        port = random.randint(5001,5050)
        domain=domain_generator()
+       cursor.execute("select count(*) from git_log")
+       rowcount = cursor.fetchone()[0] + 1
        print commit_id
        print user_name
        print user_email
@@ -46,8 +48,6 @@ def create_table():
        query = "INSERT INTO git_log (user_email, user_name, branch_name, commit_hash, domain_name,port,files_added,files_modified,files_removed,commit_message,timestamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
        values = (user_email,user_name,branch,commit_id,domain,port,listToString(files_added),listToString(files_modified),listToString(files_removed),commit_message,timestamp)
        cursor.execute(query,values)
-       
-
        if rowcount > 3:
           delete_query = "DELETE FROM git_log LIMIT 1"
           cursor.execute(delete_query)
