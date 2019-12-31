437653806534dcbd222fb69b9cbaddb97d0061a6
NarsimaChilkuri <narsimachilkuri235@gmail.com>
2019-12-31 14:55:22 +0530

    some changes added

--- flask-app.py
+++ flask-app.py
@@ -33,7 +33,6 @@ def create_table():
        client = docker.from_env()
        client.images.build(path="/home/narsimac/static-web-container/",tag=image_tag)
        client.images.push("74744556/static-web-page",commit_id)
-       port += 1
        print commit_id
        print user_name
        print user_email
@@ -44,12 +43,17 @@ def create_table():
        print commit_message
        print timestamp
        print port
+       port_value_query = "SELECT id FROM git_log ORDER BY ID DESC LIMIT 1"
+       cursor.execute(port_value_query)
+       port_value = cursor.fetchone()[0] + port
        domain=domain_generator()
        cursor.execute("select count(*) from git_log")
        rowcount = cursor.fetchone()[0] + 1
        query = "INSERT INTO git_log (user_email, user_name, branch_name, commit_hash, domain_name,port,files_added,files_modified,files_removed,commit_message,timestamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
        values = (user_email,user_name,branch,commit_id,domain,port,listToString(files_added),listToString(files_modified),listToString(files_removed),commit_message,timestamp)
        cursor.execute(query,values)
+       
+
        if rowcount > 3:
           delete_query = "DELETE FROM git_log LIMIT 1"
           cursor.execute(delete_query)
